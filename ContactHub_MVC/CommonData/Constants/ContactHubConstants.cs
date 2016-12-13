3b705368d2c326fa8bba038bcad6300a9b854290
muditm18@gmail.com <muditm18@gmail.com>
2016-12-13 19:19:36 +0530

    constant file added.

---
+++ ContactHub_MVC/CommonData/Constants/ContactHubConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactHub_MVC.CommonData.Constants
+{
+    public class ContactHubConstants
+    {
+        public const string EmailRegularexpression = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])$";
+        public const string CountryFilePath = @"~/CommonData/Files/countries.xml";
+    }
+}
