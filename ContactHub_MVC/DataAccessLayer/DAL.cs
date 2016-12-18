65f474b33a8ef141b5b6a78ea6671f0015d326e8
Mudit <Mudit@MUDIT-KAUSHIK>
2016-12-18 19:06:01 +0530

    api project removed. DAL layer modified.

--- ContactHub_MVC/DataAccessLayer/DAL.cs
+++ ContactHub_MVC/DataAccessLayer/DAL.cs
@@ -2,18 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
-using ContactHub_ApiCaller.ApiService;
 
 namespace ContactHub_MVC.DataAccessLayer
 {
-    public class DAL<T> : Controller
-        where T:class
+    public class DAL
     {
-        protected IApiCallerService<T> ApiManager { get; }
-        public DAL()
-        {
-            ApiManager = new ApiCallerService<T>();
-        }
     }
 }
