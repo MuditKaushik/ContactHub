6b43d1b5cf503f27995006bfcb22424582aa7080
muditm18@gmail.com <muditm18@gmail.com>
2017-03-03 18:42:45 +0530

    api project added.

---
+++ ContactHub_MVC/DataAccessLayer/API_DAL/ApiCrud.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactHub_MVC.DataAccessLayer.API_DAL
+{
+    public class ApiCrud<TRes, TReq> : IApiCrud<TReq, TRes>
+        where TRes : class
+        where TReq : class
+    {
+        var client = new HttpClient();
+        public ApiCrud()
+        {
+
+        }
+        Task<TRes> IApiCrud<TReq, TRes>.Delete(TReq model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("Object null");
+            return Task.FromResult<TRes>(null);
+        }
+
+        Task<TRes> IApiCrud<TReq, TRes>.Get(TReq model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("Object null");
+            return Task.FromResult<TRes>(null);
+        }
+
+        Task<TRes> IApiCrud<TReq, TRes>.Post(TReq model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("Object null");
+            return Task.FromResult<TRes>(null);
+        }
+
+        Task<TRes> IApiCrud<TReq, TRes>.Put(TReq model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("Object null");
+            return Task.FromResult<TRes>(null);
+        }
+    }
+}
---
+++ ContactHub_MVC/DataAccessLayer/API_DAL/IApiCrud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactHub_MVC.DataAccessLayer.API_DAL
+{
+
+    public interface IApiCrud<TReq,TRes>
+        where TReq:class
+        where TRes:class
+    {
+        Task<TRes> Get(TReq model);
+        Task<TRes> Put(TReq model);
+        Task<TRes> Post(TReq model);
+        Task<TRes> Delete(TReq model);
+    }
+}
